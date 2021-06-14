f6d9bac0489ff4811bcfbba43943e364cc78d48b
BuiThienNhan-nb <81063048+BuiThienNhan-nb@users.noreply.github.com>
2021-06-14 16:47:55 +0700

    Statistic donut chart
    - Statistic donut chart (doing)

--- lib/view/screen/statistic/statistic_screen.dart
+++ lib/view/screen/statistic/statistic_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:charts_flutter/flutter.dart' as charts;
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class StatisticScreen extends StatefulWidget {
   @override
@@ -10,8 +13,77 @@ class _StatisticScreenState extends State<StatisticScreen> {
   Widget build(BuildContext context) {
     return Container(
       child: Center(
-        child: Text('Statistic'),
+        child: DonutAutoLabelChart.withSampleData(),
       )
     );
   }
+}
+
+class DonutAutoLabelChart extends StatelessWidget {
+  final List<charts.Series> seriesList;
+  final bool animate;
+
+  DonutAutoLabelChart(this.seriesList, {this.animate});
+
+  /// Creates a [PieChart] with sample data and no transition.
+  factory DonutAutoLabelChart.withSampleData() {
+    return new DonutAutoLabelChart(
+      _createSampleData(),
+      // Disable animations for image tests.
+      animate: false,
+    );
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return new charts.PieChart(seriesList,
+        animate: animate,
+        // Configure the width of the pie slices to 60px. The remaining space in
+        // the chart will be left as a hole in the center.
+        //
+        // [ArcLabelDecorator] will automatically position the label inside the
+        // arc if the label will fit. If the label will not fit, it will draw
+        // outside of the arc with a leader line. Labels can always display
+        // inside or outside using [LabelPosition].
+        //
+        // Text style for inside / outside can be controlled independently by
+        // setting [insideLabelStyleSpec] and [outsideLabelStyleSpec].
+        //
+        // Example configuring different styles for inside/outside:
+        //       new charts.ArcLabelDecorator(
+        //          insideLabelStyleSpec: new charts.TextStyleSpec(...),
+        //          outsideLabelStyleSpec: new charts.TextStyleSpec(...)),
+        defaultRenderer: new charts.ArcRendererConfig(
+            arcWidth: 60,
+            arcRendererDecorators: [new charts.ArcLabelDecorator()]));
+  }
+
+  /// Create one series with sample hard coded data.
+  static List<charts.Series<Habit, String>> _createSampleData() {
+    final data = [
+      new Habit("Hoàn thành", 75),
+      new Habit("Không hoàn thành", 25),
+      new Habit("Hủy", 5),
+    ];
+
+    return [
+      new charts.Series<Habit, String>(
+        id: 'Sales',
+        domainFn: (Habit habit, _) => habit.type,
+        measureFn: (Habit habit, _) => habit.number,
+        data: data,
+        // Set a label accessor to control the text of the arc label.
+        labelAccessorFn: (Habit row, _) => '${row.type}: ${row.number}',
+      )
+    ];
+  }
+}
+
+/// Sample linear data type.
+class Habit {
+  final String type;
+  final int number;
+
+  Habit(this.type, this.number);
 }
